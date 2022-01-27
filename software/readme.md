0174b0b4d0a45ec3ec44929fee18e57d8c237c24
pablo <pablofelgueres@gmail.com>
2022-01-27 14:52:16 -0500

    [WIP] Add robust CV with annotations

--- software/readme.md
+++ software/readme.md
@@ -14,8 +14,20 @@ Camera is 3-channeled, 960 x 1280 pixels, 8-bit per pixel.
 
 Mushrooms endpoint returns vector of found mushrooms.
 
-Each vector is encoded as 3 or 4 element floating-point vector (x,y,radius) or (x,y,radius,votes)
+Each vector is encoded as 3 or 4 element floating-point vector (x,y,radius).
 
-CV2 is BGR (not RGB) 
+CV2 is BGR (not RGB).
 
- 
+For HSV, hue range is [0,179], saturation range is [0,255], and value range is [0,255].  
+
+### Methodology to extract mushroom from image
+0. Receive request from microcontroller endpoint
+1. Take 1 frame from video
+2. Convert image from BGR to HSV color-space
+3. Threshold HSV image for a range of mushroom colors
+4. Localize mushrooms using connected components algorithm
+5. Get statistics from connected components (centroids, area, components)
+6. Draw square around each mushroom and provide stats
+7. Send single mushroom statistics to robot (x,y,radius) 
+
+
--- software/vision/service.py
+++ software/vision/service.py
@@ -1,9 +1,10 @@
 '''Service file with methods to process raw images from camera
 '''
 
+from cgitb import grey
 import cv2
 import numpy as np
-from constants import MINRADIUS, MAXRADIUS, DP, MINDIST, BLUE_COLOR, GREEN_COLOR, THICKNESS, FILL, FIFTY
+from constants import MINRADIUS, MAXRADIUS, DP, MINDIST, BLUE_COLOR, GREEN_COLOR, THICKNESS, FILL, FIFTY, LOW_BROWN_HSV, HIGH_BROWN_HSV
 
 def get_n_frame(n_frame):
     '''Returns n_frame from videocam 
@@ -56,3 +57,38 @@ def get_mushrooms_coordinates(img):
         print ('No shrooms in here')
     
     return shroom_caps
+
+def mask_with_color(img):
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_color = np.uint8([[[159,108,192]]])
+    upper_color = np.uint8([[[84,67,103]]])
+    lower_color_hsv = cv2.cvtColor(lower_color, cv2.COLOR_BGR2HSV)
+    upper_color_hsv = cv2.cvtColor(upper_color, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_img,lower_color_hsv,upper_color_hsv)
+    print('Color Mask Works: {}'.format(1 if 0>=sum(sum(mask)) else 0))
+    result = cv2.bitwise_and(img, img, mask = mask)
+    return result 
+
+def get_mushrooms_with_connected_components():
+    original_img = cv2.imread('./data/train/2.jpeg')
+    img = mask_with_color(original_img)
+    gray = image_to_grayscale(img)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    output = cv2.connectedComponentsWithStats(thresh, 4, cv2.CV_32S)
+    (numLabels, labels, stats, centroids) = output
+    # Filter connected components by area
+    output_img = gray.copy()
+    print('Identified components: {}'.format(len(labels)))
+    for i in range(1, numLabels):
+        x = stats[i, cv2.CC_STAT_LEFT]
+        y = stats[i, cv2.CC_STAT_TOP]
+        w = stats[i, cv2.CC_STAT_WIDTH]
+        h = stats[i, cv2.CC_STAT_HEIGHT]
+        area = stats[i, cv2.CC_STAT_AREA]
+        (cX, cY) = centroids[i]
+        if area > 200:
+            cv2.rectangle(output_img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.circle(output_img, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+    
+    cv2.imwrite('./00_test.jpg', output_img)
+    cv2.imwrite('./00_graypost.jpeg', gray)
